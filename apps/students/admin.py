0d19602dcda048ed91ec8ffad6a40e86551beefa
Ravi-Akagra <79597232+Ravi-Akagra@users.noreply.github.com>
2022-10-01 10:32:36 +0530

    the entire code

---
+++ apps/students/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+# from .models import Student
+
+# class StudentAdmin(admin.ModelAdmin):
+#     site_header = "Student Admin"
+#     site_title = "Student Admin Area"
+#     index_title = "Welcome to Student Admin Area"
+
+# Student_admin_site = StudentAdmin(name="Student_admin")
+# Student_admin_site.regis
+
+# Register your models here.
